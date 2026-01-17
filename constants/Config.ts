717edef1dbef33d83e01439196fcc15d6c885b50
Jay_Panchal <trywebsiteapp56@gmail.com>
2026-01-17 10:45:25 +0530

    Fix the issues that come in the Notification Logic

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://192.168.0.132:4000";    
-export const API_BASE_URL = "https://cms-backend-pbfz.onrender.com";
+export const API_BASE_URL = "https://cms-backend-taupe.vercel.app/";
