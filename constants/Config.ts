a23e0b3e5827b50ac255e5d4398d9e2830c4dd6a
Jay_Panchal <trywebsiteapp56@gmail.com>
2026-02-09 10:12:28 +0530

    Change the backend deployment URL

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "http://192.168.0.132:4000";    
-export const API_BASE_URL = "https://cms-backend-taupe.vercel.app/";
+// export const API_BASE_URL = "http://192.168.0.132:4000";
+export const API_BASE_URL = "https://cms-backend-blond-one.vercel.app/";
